ce47d83abdcc47cf0aebea9bcdf2b43ec068e2fd
Iurii Bogdanov <iurii.bogdanov@check24.de>
2022-11-10 10:58:00 +0100

    Add Title

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package go_style
+
+import (
+    "io"
+    "strings"
+)
+
+func Title(w io.Writer, s string) (n int, err error) {
+    var output string
+    output += "\n"
+    output += "\u001B[33m" + strings.TrimSpace(s) + "\u001B[0m\n"
+    output += "\u001B[33m" + strings.Repeat("=", len(s)-1) + "\u001B[0m\n"
+    output += "\n"
+
+    return w.Write([]byte(output))
+}
