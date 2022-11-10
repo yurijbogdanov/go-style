93133c47f8cb902a52bfcf9110da39a65241b6d2
Iurii Bogdanov <iurii.bogdanov@check24.de>
2022-11-10 10:58:20 +0100

    Add Section

--- main.go
+++ main.go
@@ -14,3 +14,13 @@ func Title(w io.Writer, s string) (n int, err error) {
 
     return w.Write([]byte(output))
 }
+
+func Section(w io.Writer, s string) (n int, err error) {
+    var output string
+    output += "\n"
+    output += "\u001B[33m" + strings.TrimSpace(s) + "\u001B[0m\n"
+    output += "\u001B[33m" + strings.Repeat("-", len(s)-1) + "\u001B[0m\n"
+    output += "\n"
+
+    return w.Write([]byte(output))
+}
